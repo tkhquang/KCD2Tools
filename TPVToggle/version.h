e8f1ff16582a135d3f46723ab5966fcf01c5f781
Quang Trinh <khacquang.trinh@gmail.com>
2025-03-29 03:36:17 +0700

    fix(actions): update script logic

--- TPVToggle/version.h
+++ TPVToggle/version.h
@@ -16,7 +16,7 @@
 // ---------------------------------------------------------------------------
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 2
-#define VERSION_PATCH 2
+#define VERSION_PATCH 1
 // ---------------------------------------------------------------------------
 
 // Derived version components - DO NOT MODIFY DIRECTLY
